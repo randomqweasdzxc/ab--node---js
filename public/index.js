d551324f090522dbf116ff59bfb3d5471cc2a16f
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-16 15:54:38 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,27 +1,82 @@
-var socket;
-socket = io.connect('http://192.169.7.33:80/')
-socket.on('mouse', newMsg);
-function newMsg(idx, data){
-    console.log(idx);
-    console.log(data);
-}
-socket.on('keyB', newMsg1);
-function newMsg1(idx, data){
-    console.log(idx);
-    console.log(data);
-}
+//C
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
+let canvasPos = canvas.getBoundingClientRect();
+console.log(canvasPos);
+//S
+var socket;
+socket = io.connect('http://148.71.6.83:80/');
+//S.CON
+console.log(socket);
+socket.on('newPlayer', newPMsg);
+function newPMsg(idx, rndInt, allPlayers){
+    console.log(allPlayers);
+    console.log('New Player: '+idx+' going to: '+rndInt);
+    players.push(new Player(rndInt, rndInt));
+}
+//PLAYEWR
+const players = [];
+const cellSize = 100;
+let frame = 0;
+
+class Player {
+    constructor(horizontalPosition,verticalPosition){
+        this.x = -cellSize;
+        this.y = verticalPosition;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.speed = 2;
+        this.goingTo = horizontalPosition;
+    }
+    update() {
+        if(this.x < this.goingTo){
+            this.x += this.speed;
+        }       
+    }
+    draw() {
+        ctx.fillStyle = "rgb(204, 102, 0)";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+function handlePlayer() {
+    for (let i = 0; i< players.length; i++) {
+        players[i].update();
+        players[i].draw();
+    }   
+}
+function animate() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    handlePlayer();
+    frame++;
+    //console.log(frame);
+    requestAnimationFrame(animate);
+          
+}
+animate();
+//K
+function keydown(e) {
+    console.log(e.keyCode);
+    var data = e.keyCode;
+    //K.S
+    socket.emit('keyB1', data);
+}
+document.addEventListener('keydown', keydown);
+//K.S
+socket.on('keyB2', newMsgk);
+function newMsgk(idx, data){
+    console.log(idx);
+    console.log(data);
+}
+//M
 const mouse = {
     x: undefined,
     y: undefined,
     width: 0.1,
     height: 0.1,
 }
-let canvasPos = canvas.getBoundingClientRect();
-console.log(canvasPos);
+//M.C
 canvas.addEventListener('mousemove', function(e){
     mouse.x = e.x - canvasPos.left;
     mouse.y = e.y - canvasPos.top;
@@ -30,12 +85,20 @@ canvas.addEventListener('mousemove', function(e){
         x: mouse.x,
         y: mouse.y
     }
+    //M.S
     socket.emit('mouse', data);
 })
 canvas.addEventListener('mouseleave', function(){
     mouse.x = undefined;
     mouse.y = undefined;
 })
+//M.S
+socket.on('mouse', newMsg);
+function newMsg(idx, data){
+    console.log(idx);
+    console.log(data);
+}
+//q
 function collision(first, second){
     if(
         !(  first.x > second.x + second.width ||
@@ -47,9 +110,3 @@ function collision(first, second){
 
     };
 };
-function keydown(e) {
-    console.log(e.keyCode);
-    var data = e.keyCode;
-    socket.emit('keyB', data);
-}
-document.addEventListener('keydown', keydown);
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+var express = require('express');
 var app = express();
 var server = app.listen(80);
 app.use(express.static('public'));
-console.log("hello");
 var socket = require('socket.io');
 var io = socket(server);
+console.log("hello");
+const allPlayers = [];
 io.sockets.on('connection', newConnection);
 function newConnection(socket){
-    console.log('new connection: '+ socket.id);
+    let idx = socket.id;
+    socket.on('disconnect', function() {
+        console.log('Got disconnect: '+idx);
+        var i = allPlayers.indexOf(socket.id);
+        allPlayers.splice(i, 1);
+     });
+    allPlayers.push(socket.id);
+    const rndInt = Math.floor(Math.random() * 600) + 100;
+    console.log('new connection: '+ socket.id + ' going to:' + rndInt);
+    console.log(allPlayers);
+    io.sockets.emit('newPlayer', idx, rndInt);
     socket.on('mouse', mouseMsg);
     function mouseMsg(data) {
         idx = socket.id;
@@ -14,11 +26,11 @@ function newConnection(socket){
         console.log(data);
         socket.broadcast.emit('mouse', idx, data);
     }
-    socket.on('keyB', keyMsg);
+    socket.on('keyB1', keyMsg);
     function keyMsg(data){
         idx = socket.id;
-        console.log('id: ' + idx);
+        console.log('id: ' + socket.id + ' pressedKey');
         console.log(data);
-        socket.broadcast.emit('keyB', idx, data);
+        socket.broadcast.emit('keyB2', idx, data);
     }
 }
