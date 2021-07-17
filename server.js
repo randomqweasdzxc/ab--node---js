13f9af02521d6b213a9dbd593fe80fe1b034042d
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-17 23:55:56 +0100

    Update server.js
    useless object

--- server.js
+++ server.js
@@ -5,15 +5,30 @@ app.use(express.static('public'));
 var socket = require('socket.io');
 var io = socket(server);
 console.log("hello");
-let tpdata = [];
+let pids = [];
+let pxs = [];
+let pys = [];
+let ptdb = {
+    ids:0,
+    xs:0,
+    ys:0
+};
 io.sockets.on('connection', newConnection);
 function newConnection(socket){
     socket.on('disconnect', function() {
         console.log('disconnect: ' + socket.id);
-        tpdata.splice(tpdata.indexOf(socket.id), 1);
+        pxs.splice(pids.indexOf(socket.id), 1);
+        pys.splice(pids.indexOf(socket.id), 1);
+        pids.splice(pids.indexOf(socket.id), 1);
     });
-    const rndX = Math.floor(Math.random() * 600) + 100;
-    tpdata.push(socket.id);
-    console.log(tpdata);
-    io.sockets.emit('newPlayer', tpdata);
+    pids.push(socket.id);
+    ptdb.ids = pids;
+    let rndX = Math.floor(Math.random() * 600) + 100;
+    pxs.push(rndX);
+    ptdb.xs = pxs;
+    let rndY = Math.floor(Math.random() * 600) + 100;
+    pys.push(rndY);
+    ptdb.ys = pys;
+    console.log(ptdb);
+    io.sockets.emit('newPlayer', pxs, pys);
 }
