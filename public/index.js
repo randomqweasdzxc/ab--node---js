94e2789a211683eceb6f296977b727b9884169e3
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-18 21:22:36 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,47 +1,50 @@
-//C
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 800;
-let canvasPos = canvas.getBoundingClientRect();
-//S
-var socket;
-socket = io.connect('http://localhost:80/');
-//S.CON
-socket.on('newPlayer', newPMsg);
-function newPMsg(pxs, pys){
-    console.log(pxs);
-    console.log(pys);
-    for (let i = 0; i< pxs.length; i++) {
-        players.push(new Player(pxs[i], pys[i]));
-        console.log(players);
-    } 
-}
-//PLAYEWR
-let players = [];
-const cellSize = 100;
-
+const canvas = document.getElementById('canvas1')
+var ctx = canvas.getContext('2d')
+canvas.width = 800
+canvas.height = 800
+let clients = []
+const cellSize = 100
 class Player {
     constructor(horizontalPosition,verticalPosition){
-        this.x = horizontalPosition;
-        this.y = verticalPosition;
-        this.width = cellSize;
-        this.height = cellSize;
+        this.x = horizontalPosition
+        this.y = verticalPosition
+        this.width = cellSize
+        this.height = cellSize
     }
     draw() {
-        ctx.fillStyle = "rgb(204, 102, 0)";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = "rgb(204, 102, 0)"
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 function handlePlayer() {
-    for (let i = 0; i< players.length; i++) {
-        players[i].draw();
+    for (let i = 0; i< clients.length; i++) {
+        clients[i].draw()
     }   
 }
 function animate() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    handlePlayer();
-    requestAnimationFrame(animate);
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    handlePlayer()
+    requestAnimationFrame(animate)
           
 }
-animate();
+animate()
+//NODE
+var socket = io.connect('http://148.71.6.83:80/')
+socket.on('otherclients', drawothers)
+function drawothers(sxa, sya) {
+    for (let i = 0; i< sxa.length; i++) {
+        clients.push(new Player(sxa[i], sya[i]))
+    }
+}
+socket.on('selfdraw', selfdrawing)
+function selfdrawing(sx, sy){
+    clients.push(new Player(sx, sy))
+}
+socket.on('connected', newclientdraw)
+function newclientdraw(sx, sy) {
+    clients.push(new Player(sx, sy))
+}
+socket.on('disconnected', clientpopdraw)
+function clientpopdraw(sx) {
+    clients.splice(sx, 1)
+}
