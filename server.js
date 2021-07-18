b274ba712c78e0e0d6411651935f4fb802271015
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-18 21:21:49 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-var express = require('express');
-var app = express();
-var server = app.listen(80);
-app.use(express.static('public'));
-var socket = require('socket.io');
-var io = socket(server);
-console.log("hello");
-let pids = [];
-let pxs = [];
-let pys = [];
-let ptdb = {
-    ids:0,
-    xs:0,
-    ys:0
-};
-io.sockets.on('connection', newConnection);
+const express = require('express')
+const app = express()
+const server = app.listen(80)
+app.use(express.static('public'))
+const socket = require('socket.io')
+const io = socket(server)
+console.log("hello")
+const rdni = require('./commun')
+let clientid = []
+let clientpx = []
+let clientpy = []
+io.on('connection', newConnection)
 function newConnection(socket){
+    console.log(`connected ${socket.id}`)
+    socket.emit('otherclients', clientpx, clientpy)
+    clientid.push(socket.id)
+    console.log(clientid)
+    clientpx.push(rdni())
+    console.log(clientpx)
+    clientpy.push(rdni())
+    console.log(clientpy)
+    socket.emit('selfdraw', clientpx[clientid.indexOf(socket.id)], clientpy[clientid.indexOf(socket.id)])
+    socket.broadcast.emit('connected', clientpx[clientid.indexOf(socket.id)], clientpy[clientid.indexOf(socket.id)])
     socket.on('disconnect', function() {
-        console.log('disconnect: ' + socket.id);
-        pxs.splice(pids.indexOf(socket.id), 1);
-        pys.splice(pids.indexOf(socket.id), 1);
-        pids.splice(pids.indexOf(socket.id), 1);
-    });
-    pids.push(socket.id);
-    ptdb.ids = pids;
-    let rndX = Math.floor(Math.random() * 600) + 100;
-    pxs.push(rndX);
-    ptdb.xs = pxs;
-    let rndY = Math.floor(Math.random() * 600) + 100;
-    pys.push(rndY);
-    ptdb.ys = pys;
-    console.log(ptdb);
-    io.sockets.emit('newPlayer', pxs, pys);
+        console.log(`disconnected ${socket.id}`)
+        socket.broadcast.emit('disconnected', clientid.indexOf(socket.id))
+        clientpx.splice(clientid.indexOf(socket.id),1)
+        console.log(clientpx)
+        clientpy.splice(clientid.indexOf(socket.id),1)
+        console.log(clientpy)
+        clientid.splice(clientid.indexOf(socket.id),1)
+        console.log(clientid)
+    })
 }
+
