7af57c94886d581565c961d68e3d6f1f666de180
randomqweasdzxc <vinhoecola@gmail.com>
2021-07-21 18:44:11 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas1')
-var ctx = canvas.getContext('2d')
+let canvas = document.getElementById('canvas1')
+let ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 800
 let clients = []
@@ -10,6 +10,7 @@ class Player {
         this.y = verticalPosition
         this.width = cellSize
         this.height = cellSize
+        this.speed = 2
     }
     draw() {
         ctx.fillStyle = "rgb(204, 102, 0)"
@@ -19,17 +20,20 @@ class Player {
 function handlePlayer() {
     for (let i = 0; i< clients.length; i++) {
         clients[i].draw()
-    }   
+    }
 }
 function animate() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
     handlePlayer()
     requestAnimationFrame(animate)
-          
 }
 animate()
 //NODE
 var socket = io.connect('http://localhost:80/')
+socket.on('disconnected', clientpopdraw)
+function clientpopdraw(sx) {
+    clients.splice(sx, 1)
+}
 socket.on('otherclients', drawothers)
 function drawothers(sxa, sya) {
     for (let i = 0; i< sxa.length; i++) {
@@ -37,14 +41,43 @@ function drawothers(sxa, sya) {
     }
 }
 socket.on('selfdraw', selfdrawing)
-function selfdrawing(sx, sy){
-    clients.push(new Player(sx, sy))
+function selfdrawing(selfx, selfy){
+    clients.push(new Player(selfx, selfy))
 }
 socket.on('connected', newclientdraw)
 function newclientdraw(sx, sy) {
     clients.push(new Player(sx, sy))
 }
-socket.on('disconnected', clientpopdraw)
-function clientpopdraw(sx) {
-    clients.splice(sx, 1)
+socket.on('serverkeyoutput', clientupdatedraw)
+function clientupdatedraw(k, sid, sn) {
+    console.log(k, sid, sn)
+    console.log(clients[sid].x)
+    if(k=== 37){
+        while(clients[sid].x != sn && sn  >= 0){
+            clients[sid].x --
+        }
+    }
+    if(k=== 38){
+        while(clients[sid].y != sn && sn  >= 0){
+            clients[sid].y --
+        }
+    }
+    if(k=== 39){
+        while(clients[sid].x != sn && sn  <= 800){
+            clients[sid].x ++
+        }
+    }
+    if(k=== 40){
+        while(clients[sid].y != sn && sn  <= 800){
+            clients[sid].y ++
+        }
+    }
 }
+//K up38 down40 left37 right39
+function keydown(e) {
+    console.log(e.keyCode);
+    if(e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40){
+        socket.emit('clientkeyinput', e.keyCode);
+    }
+}
+document.addEventListener('keydown', keydown);
